37f534747896a522b9b527d90706a5286c372d4f
Alexey Valitov <alexeyvalitov28@gmail.com>
2023-07-31 13:02:23 +0500

    Rework commander

--- internal/app/commands/get.go
+++ internal/app/commands/get.go
@@ -3,19 +3,7 @@ package commands
 import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 
 func (c *Commander) Get(inputMessage *tgbotapi.Message) {
-	outputMsgText := "Here all the products: \n\n"
-
-	products := c.productService.List()
-	for _, p := range products {
-		outputMsgText += p.Title
-		outputMsgText += "\n"
-	}
-
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, outputMsgText)
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "TBD")
 
 	c.bot.Send(msg)
-}
-
-func init() {
-	registeredCommands["get"] = (*Commander).Get
 }
--- internal/app/commands/help.go
+++ internal/app/commands/help.go
@@ -10,7 +10,3 @@ func (c *Commander) Help(inputMessage *tgbotapi.Message) {
 
 	c.bot.Send(msg)
 }
-
-func init() {
-	registeredCommands["help"] = (*Commander).Help
-}
